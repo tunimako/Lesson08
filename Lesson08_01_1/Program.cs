efc72ac4e914a38bcd1e5fb0587d85b18dfcb861
tunimako <112893922+tunimako@users.noreply.github.com>
2022-09-21 18:45:02 +0300

    Update (2022.09.21)

--- Lesson08_01_1/Program.cs
+++ Lesson08_01_1/Program.cs
@@ -1,7 +1,6 @@
 ﻿// ** 1 UŽDUOTIS **
 
-//Sukurti struktūrą Saskaita.
-//Public laukai gavejas , siuntejas, moketiViso, saskaitosNumeris.
+//Sukurti struktūrą Saskaita. Public laukai gavejas , siuntejas, moketiViso, saskaitosNumeris.
 //Visi laukai tik skaitymui, perduodami per konstruktorių išskyrus saskaitosNumeris , jis generuojasi automatiškai konstruktoriuje (NR_metai_menuo_diena_01)
 
 using System;
